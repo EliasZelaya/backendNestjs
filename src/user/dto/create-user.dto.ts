ea03559457731aff625baffba69919fdeefab8f3
EliasZelaya <00146322@uca.edu.sv>
2024-11-10 19:48:24 -0600

    add create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, Length, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @Length(4, 16)
+  readonly name: string;
+
+  @IsNotEmpty()
+  @Length(4, 16)
+  readonly lastName: string;
+
+  @IsNotEmpty()
+  @Length(10, 24)
+  readonly email: string;
+
+  @IsNotEmpty()
+  @MinLength(5)
+  readonly password: string;
+}
